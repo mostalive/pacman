fec8d8e2fa4556cf4d916f7c48fefcc8acc76e13
Rob Westgeest <rob@sonic.(none)>
2010-01-06 19:50:48 +0100

    ẗurning on tick

--- packman.rb
+++ packman.rb
@@ -2,12 +2,13 @@ require 'test/unit'
 
 
 PACMANLOOKSUP = 'V'
+PACMANLOOKSLEFT = '>'
 PACMANWASHERE = ' '
 
-class PacmanTest < Test::Unit::TestCase
+class Field
     attr_accessor :field, :pacmanRow    
-    
-    def setup
+
+    def initialize
         self.field = [['.','.','.'],
                   ['.','V','.'],
                   ['.','.','.']]
@@ -23,22 +24,39 @@ class PacmanTest < Test::Unit::TestCase
         self.pacmanRow -= 1 
         field[pacmanRow][1] = PACMANLOOKSUP
     end
+    
+    def left
+        field[pacmanRow][1] = PACMANLOOKSLEFT
+    end
+end
 
+class PacmanTest < Test::Unit::TestCase
+
+    attr_accessor :field    
+    
+    def setup
+        self.field = Field.new
+    end
+    
     def test_packman_looks_up_in_center_of_field
-        assert_equal PACMANLOOKSUP, in_field(1,1)
+        assert_equal PACMANLOOKSUP, field.in_field(1,1)
     end
     
     def test_packman_moves_up
-        tick
-        assert_equal PACMANLOOKSUP, in_field(0,1)
-        assert_equal PACMANWASHERE, in_field(1,1)
+        field.tick
+        assert_equal PACMANLOOKSUP, field.in_field(0,1)
+        assert_equal PACMANWASHERE, field.in_field(1,1)
     end
     
     def test_wraps_around_when_at_top
-        tick
-        tick
-        assert_equal PACMANLOOKSUP, in_field(2,1)
-        assert_equal PACMANWASHERE, in_field(0,1)
+        field.tick
+        field.tick
+        assert_equal PACMANLOOKSUP, field.in_field(2,1)
+        assert_equal PACMANWASHERE, field.in_field(0,1)
     end
     
+    def test_change_direction
+        field.left
+        assert_equal PACMANLOOKSLEFT, field.in_field(1,1)
+    end
 end
